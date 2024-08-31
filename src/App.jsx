02df5d193ae3e3ad8b17c027a41ddc643e457c8e
NuttavichBig <nuttavich.si@gmail.com>
2024-08-31 20:07:46 +0700

    Decoration

--- src/App.jsx
+++ src/App.jsx
@@ -39,10 +39,11 @@ function hdlSubmit(e){
   return (
     <>
     <div className="flex justify-center flex-col items-center min-h-screen">
-    <div className="w-1/4">
+    <div className="w-1/4 border px-16 py-8">
 
-    <h1 className="text-3xl">Select Topping</h1>
-    <form className="w-full bg-red-300 p-4 border-2 border-sky-500" onSubmit={hdlSubmit}>
+    <h1 className="text-3xl">Select Topping...</h1>
+    <br/>
+    <form className="w-full bg-blue-100 p-4 border-2 border-gray-700" onSubmit={hdlSubmit}>
     {toppings.map((el)=>(<div className="flex justify-between shadow-md border border-black p-1.5">
       <label><input type="checkbox"name={el.name} onChange={(e)=>totalPriceAdjust(el.price,e)}/> {el.name}</label>
       <p>{el.price.toFixed(2)}</p>
@@ -50,8 +51,8 @@ function hdlSubmit(e){
     <div className="flex justify-between"><p>Total</p><p>{price}</p></div>
     <button className="bg-white border-2 border-black w-">Check out</button>
     </form>
-    {checkList.finalList.map((el,index)=><div className="flex justify-between shadow-md border border-black p-1.5"><p>{el}</p><p>${checkList.finalPrice[index].toFixed(2)}</p></div>)}
-    {checkList.total==0?<></>:<div><p>Total</p><p>{checkList.total}</p></div>}
+    {checkList.finalList.map((el,index)=><div className="flex justify-between shadow-md border border-gray-400 m-1 p-1.5 bg-green-200"><p>{el}</p><p>${checkList.finalPrice[index].toFixed(2)}</p></div>)}
+    {checkList.total==0?<></>:<div className="flex justify-between m-2 bg-red-400 p-1.5"><p>Total</p><p>{checkList.total}</p></div>}
     </div>
     </div>
     
